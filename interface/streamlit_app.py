917594c1e9f7ef28603c111652d480892ffe07f4
Chadi Ouhabi <ch.ouhabi@hotmail.com>
2025-05-27 17:56:54 +0200

    màj

--- interface/streamlit_app.py
+++ interface/streamlit_app.py
@@ -6,7 +6,7 @@ import os
 
 # === Chargement des artefacts ===
 model_path = os.path.join("artifacts", "best_model_lgbm_optimized.pkl")
-preprocessor_path = os.path.join("artifacts", "preprocessor_custom.pkl")
+preprocessor_path = os.path.join("artifacts", "custom_preprocessing.pkl")
 features_path = os.path.join("artifacts", "top_10_features.pkl")
 
 try:
